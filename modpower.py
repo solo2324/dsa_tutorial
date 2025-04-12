ce1e3b594cc4ae661cf20b71e1c43b051da5994f
Solome Bereket <167538300+solo2324@users.noreply.github.com>
2025-04-12 13:26:03 +0300

    Create modpower.py

---
+++ modpower.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    a=int(input().strip())
+    b=int(input().strip())
+    m=int(input().strip())
+    result_power= pow(a,b)
+    print (result_power)
+    result_power=pow(a,b,m)
+    print (result_power)
